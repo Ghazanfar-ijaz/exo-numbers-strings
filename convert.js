c72717532ccd06e95a4e86efa335b00c70c678bf
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-02-17 11:09:57 +0100

    done

---
+++ convert.js
@@ -0,0 +1,36 @@
+
+let a = 10
+let b = 15
+let c = 16
+let d = 5005
+let e = 52390903
+
+function binaire(f) {
+  console.log(f.toString(2))
+}
+
+binaire(a)
+binaire(b)
+binaire(c)
+binaire(d)
+binaire(e)
+
+function octal(o) {
+  console.log(o.toString(8))
+}
+
+octal(a)
+octal(b)
+octal(c)
+octal(d)
+octal(e)
+
+function hexadecimal(h) {
+  console.log(h.toString(16))
+}
+
+hexadecimal(a)
+hexadecimal(b)
+hexadecimal(c)
+hexadecimal(d)
+hexadecimal(e)
---
+++ decimal.js
@@ -0,0 +1,5 @@
+let nombres = [0x123, 0123, 0b10011001, 0xdeadbeef, 0xea7beef, 0b1111111111111111, 0777]
+
+for (let i = 0; i < 7; i++) {
+  console.log(nombres[i].toString(10))
+}
