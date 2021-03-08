3441d0cc1a48c94c69845fc58b0a1628aae4fdfe
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-03-08 22:25:34 +0100

    Update decimal.js

--- decimal.js
+++ decimal.js
@@ -1,3 +1,5 @@
+// Un programme qui affiche sur la console, les nombres suivants en base 10 (c'est à dire en décimal)
+
 let nombres = [0x123, 0123, 0b10011001, 0xdeadbeef, 0xea7beef, 0b1111111111111111, 0777]
 
 for (let i = 0; i < 7; i++) {
